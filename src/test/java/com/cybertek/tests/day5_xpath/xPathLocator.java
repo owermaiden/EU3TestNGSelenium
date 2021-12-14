8be18302fcfde5cfcbc1c82fd6e76f57645f8141
owermaiden <ower.maiden@cybertekschool.com>
2021-12-14 12:05:30 +0300

    Xpath

--- src/test/java/com/cybertek/tests/day5_xpath/xPathLocator.java
+++ src/test/java/com/cybertek/tests/day5_xpath/xPathLocator.java
@@ -16,6 +16,9 @@ public class xPathLocator {
         WebElement button2 = driver.findElement(By.xpath("//button[@name='button2']"));
         button2.click();
 
+        WebElement button2b = driver.findElement(By.xpath("//button[@onclick='button2()' and @name='button2']"));
+        button2b.click();
+
         WebElement button1 = driver.findElement(By.xpath("//*[text()='Button 1']"));
         button1.click();
 
@@ -25,7 +28,7 @@ public class xPathLocator {
         WebElement button1b = driver.findElement(By.xpath("//button[contains(text(),'Button 1')]"));
         button1b.click();
 
-        // dynamic id
+        // partial dynamic id
         WebElement button3 = driver.findElement(By.xpath("//button[starts-with(@id,'button_')]"));
         button3.click();
 
@@ -34,6 +37,17 @@ public class xPathLocator {
 
         // parent child relationship -> find p element of div which has container class
         WebElement p = driver.findElement(By.xpath("//div[@class='container']/p"));
+
+        // what if an element is not sibling or there is no parent child relationship between them
+        WebElement p1 = driver.findElement(By.xpath("(//p)[1]"));
+
+        Thread.sleep(2000);
+
+        driver.get("http://practice.cybertekschool.com");
+        WebElement a = driver.findElement(By.xpath("(//a[contains(text(),'Dynamic')])[1]"));
+        a.click();
+
+
 
         Thread.sleep(2000);
         driver.quit();
