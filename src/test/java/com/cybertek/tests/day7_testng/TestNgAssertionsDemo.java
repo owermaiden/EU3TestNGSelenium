0b88febbc871d15c20b99661bda277a2df183e33
owermaiden <ower.maiden@cybertekschool.com>
2021-12-14 18:19:09 +0300

    AfterMethod

--- src/test/java/com/cybertek/tests/day7_testng/TestNgAssertionsDemo.java
+++ src/test/java/com/cybertek/tests/day7_testng/TestNgAssertionsDemo.java
@@ -1,12 +1,29 @@
 package com.cybertek.tests.day7_testng;
 
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
 public class TestNgAssertionsDemo {
 
     @Test
     public void test1(){
-        Assert.assertEquals("title", "titl");
+
+        System.out.println("first assertion");
+        Assert.assertEquals("title", "titl");  // this lie fails...Remainer will not work...
+
+        System.out.println("first assertion");
+        Assert.assertEquals("url", "url");
+
+    }
+
+    @Test
+    public void test2(){
+        Assert.assertEquals("ssss", "ssss"); // this line will not be affected from failing of test1
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        System.out.println("close the browser");
     }
 }
