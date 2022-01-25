69e467e4fd76c92361034f2ef9c528c5a2ab2a61
owermaiden <ower.maiden@cybertekschool.com>
2022-01-25 15:06:12 +0300

    test ng report

--- src/test/java/com/cybertek/tests/day14_extent_reports/NegativeLoginTestWithReport.java
+++ src/test/java/com/cybertek/tests/day14_extent_reports/NegativeLoginTestWithReport.java
@@ -46,7 +46,7 @@ public class NegativeLoginTestWithReport extends TestBase {
         loginPage.loginBtn.click();
 
         extentLogger.info("verify page url");
-        Assert.assertEquals(driver.getCurrentUrl(),"https://qa1.vytrack.com/user/login");
+        Assert.assertEquals(driver.getCurrentUrl(),"https://qa1.vytrack.com/user/logi");
         extentLogger.pass("PASSED");
     }
 
