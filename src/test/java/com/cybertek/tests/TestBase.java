6a51c323919482732007115d5285d956ed934954
owermaiden <ower.maiden@cybertekschool.com>
2022-01-27 11:54:02 +0300

    DDF full

--- src/test/java/com/cybertek/tests/TestBase.java
+++ src/test/java/com/cybertek/tests/TestBase.java
@@ -90,7 +90,7 @@ public class TestBase {
             extentLogger.fail(result.getThrowable());
 
         }
-        Thread.sleep(2000);
+        //Thread.sleep(2000);
         Driver.closeDriver();
     }
 
